c378fbd38e0e4ccbbe70148becc4947266530c4e
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-08-12 12:10:20 +0545

    useState example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-//Class Based Component
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    // count += 1;  //wrong way
+
+    setCount(count + 1);  //correct way
+
+  }
+
+  const decrement = () => {
+    // count -= 1;  // wrong way
+
+    setCount(count - 1); //right way
+  }
+
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <h1>Welcome to my counter app</h1>
+      <p>The count is {count}</p>
+      <button onClick={increment}> + </button>
+      <button onClick={decrement}> - </button>
     </div>
   )
 }
