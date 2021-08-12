92102fea35690f90a30d419d1e91369052035135
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-08-12 10:45:31 +0545

    jsx introduction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+//BEM - Block Element Modifier
+
 function App() {
+
+  const name = "Dipendra";
+  const isMale = true;
+
+  // userdefined function
+  function getString(user) {
+    if (user) {
+      return <h1>Hello,  {user}!</h1>;
+    } else {
+      return <h1>Hello, Stranger.</h1>;
+    }
+
+  }
+
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      {getString()}
+      <h2>You are a {isMale ? "Male" : "Female"}</h2>
     </div>
   );
 }
