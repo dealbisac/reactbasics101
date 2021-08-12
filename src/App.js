48e897cea043bf3dae0ed0b7b19939bef9da6f69
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-08-12 11:08:59 +0545

    class based component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import './App.css';
-import Header from './Header';
+import React, { Component } from 'react';
 
-//BEM - Block Element Modifier
-
-function App() {
-
-  return (
-    <div className="app">
-      <Header />
-      <h1 className="app__title">This is a title</h1>
-      <h1 className="app__title--large">This is a large title</h1>
-      <h1 className="app__title--small">This is small title</h1>
-    </div>
-  );
+//Class Based Component
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
