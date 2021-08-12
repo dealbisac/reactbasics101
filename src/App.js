4710a24868ace92ecdbf80f3bf70cc7f5a1f02d5
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-08-12 14:14:16 +0545

    complete form

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
-  const [todos, setTodos] = useState([
-    "Take me there",
-    "Finish Coding",
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const [input, setInput] = useState("");
 
   const addTodo = (e) => {
-    console.log(`This is input ${input}`);
-    setTodos([...todos, input])
+    e.preventDefault();  //prevents refresh
+
+
+    // console.log(`This is input ${input}`);
+    setTodos([...todos, input]);
+    setInput("");
   }
 
 
@@ -19,8 +20,11 @@ function App() {
   return (
     <div className="app">
       <h1>Welcome to my TODO List</h1>
-      <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
-      <button onClick={addTodo}>Add Todo</button>
+      <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+
 
       <h2>List of Todos</h2>
       {todos.map((todo) => (
